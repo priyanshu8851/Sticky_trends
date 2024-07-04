3b253f5bdbb853123ebcedc6be3ead461f97f803
Priyanshu <priyanshukashyap844@gmail.com>
2024-07-05 01:39:15 +0530

    Updating amount

--- src/utils/cardData.js
+++ src/utils/cardData.js
@@ -3,272 +3,272 @@ const projects = [
     {
         name:"46 MotoRacer",
         img : '/images/46.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     
     {
         name:"AmongUs",
         img : '/images/AmongUs.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"AngrySkull",
         img : '/images/AngrySkull.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Anxiety",
         img : '/images/Anxiety.WEBP',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Batman",
         img : '/images/batman.JPG',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
-        name:"Gears",
+        name:"Gea/-",
         img : '/images/BI.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Biker",
         img : '/images/Biker2.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Biker 2",
         img : '/images/Biker3.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },{
-        name:"Riders",
+        name:"Ride/-",
         img : '/images/BikerBuddies.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Biker Pose",
         img : '/images/BikerPose1.WEBP',
-        amt:"20rs"
+        amt:"20/-"
     },{
         name:"Bitcoin",
         img : '/images/Bitcoin.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Boom",
         img : '/images/Boom.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Brain Meme",
         img : '/images/BrainMeme.JPG',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Chai Addicted",
         img : '/images/ChaiAddicted.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Coffee",
         img : '/images/Coffee.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
-        name:"Converse Shoes",
-        img : '/images/Converse.jpg',
-        amt:"20rs"
+        name:"Conve/-e Shoes",
+        img : '/images/Conve/-e.jpg',
+        amt:"20/-"
     },
     {
         name:"Cool Skull",
         img : '/images/CoolSkull.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Crazy",
         img : '/images/Crazy.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Deadpool",
         img : '/images/Deadpool.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Delhi",
         img : '/images/Delhi.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Disappear",
         img : '/images/Disappear.JPG',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Dog",
         img : '/images/Dog.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Dollar",
         img : '/images/dollar.WEBP',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Dragon",
         img : '/images/Dragon.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Elephants",
         img : '/images/Elephants.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Explore",
         img : '/images/Explore.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Fire Eye",
         img : '/images/FireEye.WEBP',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Flash",
         img : '/images/Flash.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
-        name:"Gears",
-        img : '/images/GearStructure.jpg',
-        amt:"20rs"
+        name:"Gea/-",
+        img : '/images/Gea/-tructure.jpg',
+        amt:"20/-"
     },
     {
         name:"Ghanta",
         img : '/images/Ghanta.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Ghost",
         img : '/images/Ghost.JPG',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Girl",
         img : '/images/Girl.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Goku",
         img : '/images/GokuRelax.WEBP',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Go Ninja",
         img : '/images/GoNinja.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Green Amongus",
         img : '/images/GreenAmong.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Groot",
         img : '/images/Groot.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Headphone",
         img : '/images/Headphone.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Hotdog",
         img : '/images/Hotdog.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"HotWheels",
         img : '/images/hotwheels.JPG',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Ironman",
         img : '/images/IronMan.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Jet",
         img : '/images/Jet.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"JustRide",
         img : '/images/JustRide.JPG',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Keep Going",
         img : '/images/KeepGoing.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Kick Ass",
         img : '/images/KickAss.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Maa Ki Ankh",
         img : '/images/MaaKiAnkh.WEBP',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Miss ME ?",
         img : '/images/MissMe.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"MotoCross",
         img : '/images/MotoCross.JPG',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Mr Poo",
         img : '/images/Mrpoo.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Music",
         img : '/images/Music.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"New Delhi",
         img : '/images/NewDelhi.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"No Speed Limit",
         img : '/images/NoSpeedLimit.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"On Mission",
         img : '/images/OnMission.jpg',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Panda Bubble Tea",
         img : '/images/PandaBubbleTea.JPG',
-        amt:"20rs"
+        amt:"20/-"
     },
     {
         name:"Power Drift",
         img : '/images/PowerDriftBiker.JPG',
-        amt:"20rs"
-    },
+        amt:"20/-"
+    }
     
     
     
